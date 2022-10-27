079920d8f68d1c6ce41843067988251a9fbb72b5
fajar8921 <shanum8921@gmail.com>
2022-10-27 13:44:25 +0700

    jawaban no.3

--- resources/views/mahasiswa/index.blade.php
+++ resources/views/mahasiswa/index.blade.php
@@ -1,1 +1,21 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <p>MAHASISWA</p>
+</head>
+<body>
+<li class="nav-item">
+        <a class="nav-link" href="{{ URL::to('mahasiswa/pendaftaran') }}">Pendaftaran</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ URL::to('mahasiswa/ujian') }}">Ujian</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="{{ URL::to('mahasiswa/nilai')}}">Nilai</a>
+      </li>
+    </ul>
+   </div>
+  </div>
+</nav>
+</body>
+</html>
